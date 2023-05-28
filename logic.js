a3228dcdafc2fee5ba585ae8d6093c2e9997c5bf
Sadab Azhar <sadabazhar84@gmail.com>
2023-05-28 20:08:04 +0530

    Adding js file for opening/closing  menu-bar

---
+++ logic.js
@@ -0,0 +1,9 @@
+/* -- --------Menu-bar-------- -- */
+
+var sidemanu = document.getElementById("side-menu");
+function openmenu() {
+  sidemanu.style.right = "0";
+}
+function closemenu() {
+  sidemanu.style.right = "-200px";
+}
